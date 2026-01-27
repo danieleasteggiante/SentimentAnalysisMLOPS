d6146a67445bf2ef722f28bc5bb1e9b6402b08e7
danieleasteggiante <52632176+danieleasteggiante@users.noreply.github.com>
2026-01-27 11:14:53 +0000

    removed entity

--- trainer/entity/Start_training_logs.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from config.database import Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-
-class Start_training_logs(Base):
-    __tablename__ = 'start_training_logs'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    feedback_id = Column(String(100), nullable=False)
-    created_at = Column(DateTime, default=datetime.now)
